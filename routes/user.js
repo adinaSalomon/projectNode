c3e26c858c7145f1a304b0d73d4794b6458c4769
AdinasSalomon <adinas259@gmail.com>
2021-07-16 15:10:04 +0300

    update commit

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,11 @@
 const express=require('express');
 const Pets = require('../models/Pets');
  var router=express.Router();
-const cars=["aaa","bbb","ccc"];
 const pets=require('../models/Pets');
 router.get('/',function(req,res,next){
   res.send([{name:"adina"}])
 });
-router.post('/', function (req, res, next) {
-  cars.push(req.body.name);
-  res.send(cars)
-});
+
 router.delete('/:id', function (req, res, next) {
  Pets.findOneAndDelete({id:req.params.id}).then(function(pet){
    res.send(pet);
@@ -18,11 +14,11 @@ router.delete('/:id', function (req, res, next) {
 router.put('/update/:id',function(req,rea,next){
   Pets.findOneAndUpdate({_id:req.params.id},req.body).then(
     function(pet){pets.findOne({_id:req.params.id}).then(function(pet){
-      res.send(pet);
+      res.send(pet.name);
     })}
   )
 })
-router.post('/add', function (req, res, next) {
+router.post('/addalot', function (req, res, next) {
   pets.insertMany(req.body, function (err, result) {
     if (err) {
       res.send(err);
