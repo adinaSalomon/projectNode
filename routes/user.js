773137badf5c8c6304e5cf388691ad91033909d5
AdinasSalomon <adinas259@gmail.com>
2021-07-18 13:12:26 +0300

    update2 commit

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,32 @@
 const express=require('express');
 const Pets = require('../models/Pets');
  var router=express.Router();
-const pets=require('../models/Pets');
-router.get('/',function(req,res,next){
-  res.send([{name:"adina"}])
+///יש לי בעיה הוא משום מה באמת מוחק ולא עושה שגיאה
+router.delete('/:id',async function (req, res, next) {
+  try {
+    await Pets.findByIdAndDelete(req.params.id);
+    res.status(201);
+} catch (error) {
+    res.status(500).send(error);
+}
 });
 
-router.delete('/:id', function (req, res, next) {
- Pets.findOneAndDelete({id:req.params.id}).then(function(pet){
-   res.send(pet);
- })
-});
-router.put('/update/:id',function(req,rea,next){
-  Pets.findOneAndUpdate({_id:req.params.id},req.body).then(
-    function(pet){pets.findOne({_id:req.params.id}).then(function(pet){
-      res.send(pet.name);
-    })}
-  )
+router.get('/:id',async function (req, res, next){try {
+  let pet = await Pets.findById(req.params.id);
+  res.status(200).json(pet);
+} catch (error) {
+  res.status(500).send(error.message);
+}})
+//כנל פה לא באמת מידכן לי ולא עושה שגיאה
+router.put('updatePet/:id',async function(req,rea,next){
+  try {
+    const petUpdate = await Petshop.findByIdAndUpdate(req.params.id, req.body);
+    console.log(petUpdate);
+    res.status(200).send('pet update succeed');
+} catch (error) {
+    res.status(500).send(error);
+}
+ 
 })
 router.post('/addalot', function (req, res, next) {
   pets.insertMany(req.body, function (err, result) {
@@ -27,18 +37,24 @@ router.post('/addalot', function (req, res, next) {
     }
   });
 });
-router.get('/getall', function (req, res, next) {
-  pets.find({}, function (err, result) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
+router.get('/getall', async function (req, res, next) {
+  try {
+    let pets = await Pets.find();
+    res.status(200).json(pets);
+} catch (error) {
+    res.status(500).send(error.message);
+}
 });
-router.post('/addpet', function (req, res, next) {
-  pets.create(req.body).then(function (p) {
-    res.send(p);
-  }).catch(next);
+router.post('/addpet',async function (req, res, next) {
+  const pet = new Pets({
+    name: req.body.name,
+    species: req.body.species,
+});
+try {
+    await pet.save();
+    res.status(200).json(pet);
+} catch (error) {
+    res.status(500).send(error.message);
+}
 });
 module.exports=router;
